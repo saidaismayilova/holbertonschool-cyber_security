ca3ca828d95564d660313df3f689d087c1875e54
saidaismayilova <iseide40@gmail.com>
2025-12-20 11:36:48 -0500

    salam

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "$2  $1" > tmp.sha256
-sha256sum -c tmp.sha256 &>/dev/null && echo ok || echo invalid
-rm tmp.sha256
+sha256sum -c <(echo "$2  $1") &>/dev/null && echo ok || echo invalid
