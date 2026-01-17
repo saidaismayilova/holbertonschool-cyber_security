1365b84844a1e6a1fe636a4f60d825cdbcc2a822
holbiejlln <iseide40@gmail.com>
2026-01-17 14:40:33 -0500

    salam

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <new_host> <target_url> <form_data>"
-    exit 1
-fi
-
-NEW_HOST="$1"
-TARGET_URL="$2"
-FORM_DATA="$3"
-
-curl -s -X POST \
-  -H "Host: $NEW_HOST" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "$FORM_DATA" \
-  "$TARGET_URL"
+curl -s -X POST -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" -d "$3" "$2"
