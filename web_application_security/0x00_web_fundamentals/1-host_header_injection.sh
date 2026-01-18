a13320e1785a9405a15e4895629657ee9bf4968d
holbiejlln <iseide40@gmail.com>
2026-01-18 13:03:10 -0500

    s

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -X POST -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" -d "$3" "$2"
+curl -i -X POST -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" -d "$3" "$2"
