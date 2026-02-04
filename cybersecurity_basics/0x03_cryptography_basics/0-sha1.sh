04e9583b3e2d0ed989443d908c1232156c66e2a1
holbiejlln <iseide40@gmail.com>
2026-02-04 12:17:05 -0500

    s

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Hash the password argument using SHA-1
 echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
--- cybersecurity_basics/0x03_cryptography_basics/1-sha256.sh
+++ cybersecurity_basics/0x03_cryptography_basics/1-sha256.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Hash the password argument using SHA-256
 echo -n "$1" | sha256sum | awk '{print $1}' > 1_hash.txt
