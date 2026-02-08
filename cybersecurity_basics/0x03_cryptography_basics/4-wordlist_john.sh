795de51750f9902abd88bdc1de5f4b22f31a0430
holbiejlln <iseide40@gmail.com>
2026-02-08 07:14:55 -0500

    s

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-SHA256 "$1" >/dev/null 2>&1
-john --show --format=Raw-SHA256 "$1" | awk -F: 'NF>1{print $2}' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-SHA256 "$1" | john --show --format=Raw-SHA256 "$1" | cut -d: -f2 > 4-password.txt
