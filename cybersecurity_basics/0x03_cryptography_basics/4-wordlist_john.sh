5a74b00fc7cadf185cda07bbd348effbf73bc07a
holbiejlln <iseide40@gmail.com>
2026-02-08 07:18:30 -0500

    s

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-SHA256 "$1" | john --show --format=Raw-SHA256 "$1" | cut -d: -f2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-MD5 "$1"
