db90196d7cd24ea02a97d102c37598e206dd95c3
saidaismayilova <iseide40@gmail.com>
2026-04-13 02:30:24 -0400

    salam

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
-#!bin/bash
-grep "Failed password" /var/log/auth.log | awk '{print $(NF-5)}' | sort | uniq -c | sort -nr
+#!/bin/bash
+grep -E "Failed password|Accepted password" auth.log | awk '{print $(NF-5)}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
